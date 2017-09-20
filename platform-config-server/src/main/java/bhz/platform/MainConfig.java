3fd0adfe743e45f8018d6274cbb46eca2d1554c7
baihezhuo <174754613@qq.com>
2017-09-20 17:33:27 +0800

    添加主配置类

--- platform-config-server/src/main/java/bhz/platform/MainConfig.java
+++ platform-config-server/src/main/java/bhz/platform/MainConfig.java
@@ -1,5 +1,10 @@
 package bhz.platform;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"bhz.platform.*"})
 public class MainConfig {
 
 }
