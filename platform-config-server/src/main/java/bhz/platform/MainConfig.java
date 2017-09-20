030f24a59dd6efed14be54137971f7a0aa057345
baihezhuo <174754613@qq.com>
2017-09-20 17:48:31 +0800

    1111111

--- platform-config-server/src/main/java/bhz/platform/MainConfig.java
+++ platform-config-server/src/main/java/bhz/platform/MainConfig.java
@@ -3,9 +3,9 @@ package bhz.platform;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 /**
- * <B>系统名称：</B><BR>
- * <B>模块名称：</B><BR>
- * <B>中文类名：</B><BR>
+ * <B>系统名称：</B>platform-config-server<BR>
+ * <B>模块名称：</B>platform-config-server<BR>
+ * <B>中文类名：</B>MainConfig<BR>
  * <B>概要说明：</B><BR>
  * @author bhz
  * @since 2017年9月20日 下午5:47:28
